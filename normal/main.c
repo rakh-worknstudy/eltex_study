19a0701412f05ea2a4a7cb6653f914a946229111
kfcenjoyer <rahmatovforname@gmail.com>
2023-02-22 01:53:27 +0700

    normal bez vsego [calc]

--- normal/main.c
+++ normal/main.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 #include <malloc.h>
-#include "eltex_string.h"
-#include "eltex_stack.h"
 
-#define _ELTEX_DEBUG
+// #define _ELTEX_DEBUG
+/* Тут были стэк операций на случай, если бы делал через польскую
+  нотацию и ввод выражений, но от этого пришлось отказаться, т.к.
+  мало времени. Но прикола ради это можно задебажить */
+#ifdef _ELTEX_DEBUG
+#include "eltex_stack.h"
+#include "eltex_string.h"
+#endif /* _ELTEX_DEBUG */
 
 # ifndef _ELTEX_DEBUG
 int eltex_calc_menu(void);
@@ -34,13 +39,58 @@ int main(void) {
   eltex_string_free(&string);
   printf("%s", " DONE\n");
   return 0;
-# elif
+# else
   return eltex_calc_menu();
 # endif /* _ELTEX_DEBUG */
 }
 
 # ifndef _ELTEX_DEBUG
 int eltex_calc_menu(void) {
-  
+  long double current;
+  long double result = 0.0L;
+  //  char *string = NULL;
+  char tempmore;
+  while (1) {
+    printf("Текущее значение: %Lf\n", result);
+    printf("1 - Прибавить\n" \
+           "2 - Вычесть\n"   \
+           "3 - Умножить\n"  \
+           "4 - Разделить\n" \
+           "5 - Сброс\n"     \
+           "0 - Выход\n");
+    tempmore = getchar();
+    if (tempmore == '0')
+      break;
+    if (tempmore == '5') {
+      result = 0.0L;
+      continue;
+    }
+    if (tempmore > '5' || tempmore < '0')
+      continue;
+    printf("n = ");
+    //  string = eltex_string_get(&string);
+    if (scanf("%Lf", &current) == 1) {
+      switch (tempmore) {
+        case '1':
+          result += current;
+          break;
+        case '2':
+          result -= current;
+          break;
+        case '3':
+          result *= current;
+          break;
+        case '4':
+          result /= current;
+          break;
+        default:
+          break;
+      }
+    } else {
+      printf("Плохой ввод\n");
+    }
+    getchar();
+  }
+  //  eltex_string_free(&string);
 }
 # endif /* _ELTEX_DEBUG */
