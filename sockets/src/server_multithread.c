9345d14bf5d2c386d085eba217a0e5a6d6e9bcc1
rakh-worknstudy <119973176+rakh-worknstudy@users.noreply.github.com>
2023-04-26 07:33:06 +0700

    Add files via upload

--- sockets/src/server_multithread.c
+++ sockets/src/server_multithread.c
@@ -5,18 +5,23 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <pthreads.h>
-#include <timer.h>
+#include <pthread.h>
 
 #include "lib/eltex_buffer.h"
+
+typedef struct Connection_arg {
+	int connection_fd;
+	struct sockaddr_in client_address;
+} connection_arg_t;
+unsigned char cl_iter;
+pthread_mutex_t cl_iter_lock;
 
 int server_main(void);
 int server_init(int *socket_fd, struct sockaddr_in *server_address);
 int server_loop(int socket_fd);
 
-void *connection_open(void);
-void *connection_loop(int connection_fd);
-void connection_close(connection_list_t *clients);
+int connection_wait(int socket_fd);
+void *connection_open(void *arg);
 
 int main(void) {
 	return server_main();
@@ -28,7 +33,7 @@ int server_main(void) {
 	int socket_fd, connection_fd;
 	struct sockaddr_in server_address;
 	server_init(&socket_fd, &server_address);
-
+	connection_wait(socket_fd);
 	return 0;
 }
 //	================================================================
@@ -72,23 +77,78 @@ int server_init(int *socket_fd, struct sockaddr_in *server_address) {
 //	================================================================
 //	                 NEW CONNECTION INITALIZATION
 //	================================================================
-int connection_open(void) {
+int connection_wait(int socket_fd) {
 	int connection_fd;
-	struct sockaddr_in *client_address = (struct sockaddr_in *)malloc(sizeof(struct sockaddr_in));
-	int length = sizeof(struct sockaddr_in);
+	int length = sizeof(connection_arg_t);
+	connection_arg_t *arg;
+	pthread_t sv_thread[5];
+	cl_iter = 0;
 	while (1) {
-		if ((connection_fd = accept(socket_fd, (struct sockaddr *)sockaddr_in, &length)) == -1) {
+		if (cl_iter == 5) {
+			printf("MAX CLIENTS\n");
+			while (cl_iter == 5)
+				continue;
+		}
+		printf("Allocating struct:   ");
+		arg = (connection_arg_t *)malloc(sizeof(connection_arg_t));
+		if (!arg) {
+			printf("FAIL\n");
+			continue;
+		}
+		printf("SUCCESS\n");
+		if ((connection_fd = accept(socket_fd, (struct sockaddr *)&arg->client_address, &length)) == -1) {
 			printf("New client FAILED to connect\n");
+			free(arg);
 			continue;
 		}
 		printf("New client SUCCESSFULLY connected\n");
-		break;
+		arg->connection_fd = connection_fd;
+		printf("Creating new thread: ");
+		if (pthread_create(&sv_thread[cl_iter], NULL, connection_open, (void *)arg)) {
+			printf("FAIL\n");
+			free(arg);
+			continue;
+		}
+		printf("SUCCESS\n");
+		++cl_iter;
 	}
-	return 1;
+	return 0;
 }
 
-/* CLOSING OLD CONNECTION */
-void connection_close(int connection_fd, struct sockaddr_in *client_address) {
-	free(client_address);
+void *connection_open(void *arg) {
+	/* recv & send */
+	char buffer[261];
+	int connection_fd = ((connection_arg_t *)arg)->connection_fd;
+	while (1) {
+		read(connection_fd, buffer, 261);
+		unsigned short length = eltex_strlen(buffer, 256);
+		if (length > 256) {
+			printf("================================\n");
+			printf("msg too big\n");
+			continue;
+		}
+		if (eltex_strcmp(buffer, length)) {
+			printf("================================\n");
+			printf("recv: exit\n");
+			printf("send: exit\n");
+			close(connection_fd);
+			break;
+		}
+		printf("================================\n");
+		printf("recv: %s\n", buffer);
+		buffer[length++] = 'b';
+		buffer[length++] = 'a';
+		buffer[length++] = 'c';
+		buffer[length++] = 'k';
+		buffer[length] = 0;
+		write(connection_fd, buffer, 261);
+		printf("send: %s\n", buffer);
+	}
+	while (pthread_mutex_trylock(&cl_iter_lock))
+		continue;
+	--cl_iter;
+	pthread_mutex_unlock(&cl_iter_lock);
 	close(connection_fd);
+	free(arg);
+	pthread_exit(NULL);
 }
